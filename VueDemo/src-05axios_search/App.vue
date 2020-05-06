ca7e8f8f73e6c7c75f3ca6a6a500435ff8071527
zbcoolcode <951929027@qq.com>
2020-05-06 15:51:54 +0800

    第五个版本，路由实现

---
+++ VueDemo/src-05axios_search/App.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+      <div class="container">
+        <Search></Search>
+        <Main></Main>
+      </div>
+    </div>
+</template>
+
+<script>
+  import Main from './components/main'
+  import Search from './components/search'
+  export default {
+    components:{
+      Main,Search,
+    }
+  }
+
+</script>
+
+<style>
+
+</style>
